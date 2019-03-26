e97164ae2d6deb12c06d0cf9795162b7ce5bebdc
unknown <simgedemir@sabanciuniv.edu>
2019-03-26 18:01:30 +0300

    first commit

---
+++ 432project_server/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace _432project_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logs = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listenButton = new System.Windows.Forms.Button();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.Label();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // logs
+            // 
+            this.logs.Location = new System.Drawing.Point(390, 58);
+            this.logs.Name = "logs";
+            this.logs.Size = new System.Drawing.Size(338, 550);
+            this.logs.TabIndex = 1;
+            this.logs.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 430);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 31);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 433);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // listenButton
+            // 
+            this.listenButton.Location = new System.Drawing.Point(148, 500);
+            this.listenButton.Name = "listenButton";
+            this.listenButton.Size = new System.Drawing.Size(158, 62);
+            this.listenButton.TabIndex = 5;
+            this.listenButton.Text = "Listen";
+            this.listenButton.UseVisualStyleBackColor = true;
+            this.listenButton.Click += new System.EventHandler(this.listenButton_Click);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(126, 188);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(196, 31);
+            this.passwordBox.TabIndex = 6;
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(8, 188);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(112, 25);
+            this.password.TabIndex = 7;
+            this.password.Text = "Password:";
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(148, 254);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(158, 48);
+            this.sendButton.TabIndex = 8;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 787);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.listenButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.logs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox logs;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button listenButton;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.Button sendButton;
+    }
+}
+
---
+++ 432project_server/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace _432project_server
+{
+
+    public partial class Form1 : Form
+    {
+        bool terminating = false;
+        bool listening = false;
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<Socket> socketList = new List<Socket>();
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            InitializeComponent();
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            listening = false;
+            terminating = true;
+            Environment.Exit(0);
+        }
+
+        private void listenButton_Click(object sender, EventArgs e)
+        {
+            int serverPort;
+            Thread acceptThread;
+
+            if (Int32.TryParse(textBox2.Text, out serverPort))
+            {
+                serverSocket.Bind(new IPEndPoint(IPAddress.Any, serverPort));
+                serverSocket.Listen(3);
+
+                listening = true;
+                listenButton.Enabled = false;
+                acceptThread = new Thread(new ThreadStart(Accept));
+                acceptThread.Start();
+
+                logs.AppendText("Started listening on port: " + serverPort + "\n");
+            }
+            else
+            {
+                logs.AppendText("Please check port number \n");
+            }
+        }
+        private void Accept()
+        {
+            while (listening)
+            {
+                try
+                {
+                    socketList.Add(serverSocket.Accept());
+                    logs.AppendText("A client is connected \n");
+
+          
+                }
+                catch
+                {
+                    if (terminating)
+                    {
+                        listening = false;
+                        logs.AppendText("Client is disconnected \n"); //username display
+                    }
+                    else
+                    {
+                        logs.AppendText("The socket stopped working \n");
+                    }
+                }
+            }
+        }
+    }
+}
